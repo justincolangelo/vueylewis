8e6e99a670aeeca6ece2f8b079bab3dfea639190
Justin Colangelo <justin.colangelo@garmin.com>
2019-10-08 15:37:54 -0400

    display ToDos from state in About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="vu-about">
     <h1>This is an about page</h1>
+    <ListDisplay :items="$store.getters.toDos" classes="vu-toDos" />
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import ListDisplay from '@/components/ListDisplay.vue';
+
+@Component({
+  components: {
+    ListDisplay
+  }
+})
+
+export default class About extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '@/scss/modules/_about.scss';
+</style>
